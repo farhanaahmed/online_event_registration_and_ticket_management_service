5488a29067979b5acacd7055eadcd2be393eb6c2
Farhana Ahmed <farhana.ahmed.fima@gmail.com>
2022-09-04 21:28:01 +0600

    add qr code and download button

--- src/components/EventDetails/EventDetails.js
+++ src/components/EventDetails/EventDetails.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import QRCode from 'react-qr-code';
 import { useLocation } from 'react-router-dom';
 import Header from '../Header/Header';
+import './EventDetails'
 
 const EventDetails = () => {
 
     const {state} = useLocation();
     const event = state.event;
 
-    return (
+    const onDownloadClicked = ()=>{
 
+    }
+
+    return (
         <div id='ticket'>
             <Header></Header>
             <div>
@@ -16,6 +21,9 @@ const EventDetails = () => {
                 <h3>Location : {event.location}</h3>
                 <h3>Date : {event.date}</h3>
                 <h3>Ticket Price: {event.price} BDT</h3>
+                <QRCode className='.qr-code' value={JSON.stringify(event)} />
+                <br />
+                <button onClick={onDownloadClicked}>Download Ticket</button>
             </div>
         </div>
         
