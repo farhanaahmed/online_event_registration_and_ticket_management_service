2c394c0eed04a69935ac5632efe770cdc6cf3e7c
Farhana Ahmed <farhana.ahmed.fima@gmail.com>
2022-09-04 21:49:27 +0600

    add download options

--- src/components/EventDetails/EventDetails.js
+++ src/components/EventDetails/EventDetails.js
@@ -3,6 +3,9 @@ import QRCode from 'react-qr-code';
 import { useLocation } from 'react-router-dom';
 import Header from '../Header/Header';
 import './EventDetails'
+import * as htmlToImage from 'html-to-image';
+import { jsPDF } from "jspdf";
+
 
 const EventDetails = () => {
 
@@ -10,18 +13,30 @@ const EventDetails = () => {
     const event = state.event;
 
     const onDownloadClicked = ()=>{
-
+        htmlToImage.toPng(document.getElementById("ticket"), { quality: 0.95 })
+        .then(function (dataUrl) {
+          var link = document.createElement('a');
+          link.download = 'my-image-name.jpeg';
+          const pdf = new jsPDF();          
+          const imgProps= pdf.getImageProperties(dataUrl);
+          const pdfWidth = pdf.internal.pageSize.getWidth();
+          const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+          pdf.addImage(dataUrl, 'PNG', 0, 0,pdfWidth, pdfHeight);
+          pdf.save("ticket.pdf"); 
+        });
     }
 
     return (
-        <div id='ticket'>
+        <div>
             <Header></Header>
             <div>
-                <p>{event.description}</p>
-                <h3>Location : {event.location}</h3>
-                <h3>Date : {event.date}</h3>
-                <h3>Ticket Price: {event.price} BDT</h3>
-                <QRCode className='.qr-code' value={JSON.stringify(event)} />
+                <div id='ticket'>
+                    <p>{event.description}</p>
+                    <h3>Location : {event.location}</h3>
+                    <h3>Date : {event.date}</h3>
+                    <h3>Ticket Price: {event.price} BDT</h3>
+                    <QRCode className='.qr-code' value={JSON.stringify(event)} />
+                </div>
                 <br />
                 <button onClick={onDownloadClicked}>Download Ticket</button>
             </div>
