7c205d6e172c0b095499d8ee41f49b31eafd99c9
Farhana Ahmed <farhana.ahmed.fima@gmail.com>
2022-09-18 14:42:38 +0600

    create TicketInfo.js

--- src/components/TicketInfo/TicketInfo.js
+++ src/components/TicketInfo/TicketInfo.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from '../Header/Header';
 import './TicketInfo.css'
 
 const TicketInfo = () => {
+    const[name,setName] = useState('');
+    const[phone,setPhone] = useState('');
+    const [numberOfTickets,setNumberOfTickets] = useState('');
+    const handleNameChange =(e)=>{
+        setName(e.target.value);
+    }
+
+    const handlePhoneChange =(e)=>{
+        setPhone(e.target.value);
+    }
+
+    const handleNumberOfTicketsChange =(e)=>{
+        setNumberOfTickets(e.target.value);
+    }
+    const ticketInfo = {
+        name : name,
+        phone : phone,
+        numberOfTickets : numberOfTickets,
+    }
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log(ticketInfo);
+    }
     return (
         <div>
+            <Header></Header>
             <h1>Ticket Information</h1>
+            <form onSubmit={(e) => {handleSubmit(e)}}>
+                <label>Name :</label><br/><br />
+                <input className='input' type="text" placeholder='John Doe' required onChange={(e)=>{handleNameChange(e)}} /><br/><br />
+                <label>Phone :</label><br/><br />
+                <input className='input' type="numeric" placeholder='Enter your phone number' required onChange={(e)=>{handlePhoneChange(e)}} /><br/><br />
+                <label>Number of Tickets :</label><br/><br />
+                <input className='input' type="number" min={1} placeholder='Enter number of tickets you want' required onChange={(e)=>{handleNumberOfTicketsChange(e)}} /><br/><br />
+                <input className='submit' type="submit" value="Confirm"/>  
+            </form>
         </div>
     );
 };
